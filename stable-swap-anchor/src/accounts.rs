4c3555ab26e97a966e48fb20a0f199b00fe352f0
Kakao-max <angrybird4837@gmail.com>
2024-06-02 08:22:26 -0400

    updated

--- stable-swap-anchor/src/accounts.rs
+++ stable-swap-anchor/src/accounts.rs
@@ -2,93 +2,15 @@
 
 use anchor_lang::prelude::*;
 
-/// Accounts for an [crate::initialize] instruction.
-#[derive(Accounts, Clone)]
-pub struct Initialize<'info> {
-    /// The swap.
-    #[account(signer)]
-    pub swap: AccountInfo<'info>,
-    /// The authority of the swap.
-    pub swap_authority: AccountInfo<'info>,
-    /// The admin of the swap.
-    pub admin: AccountInfo<'info>,
-    /// The A token of the swap.
-    pub token_a: InitToken<'info>,
-    /// The B token of the swap.
-    pub token_b: InitToken<'info>,
-    /// The pool mint of the swap.
-    pub pool_mint: AccountInfo<'info>,
-    /// The output account for LP tokens.
-    pub output_lp: AccountInfo<'info>,
-    /// The spl_token program.
-    pub token_program: AccountInfo<'info>,
-}
 
-/// Accounts for a [crate::deposit] instruction.
-#[derive(Accounts, Clone)]
-pub struct Deposit<'info> {
-    /// The context of the user.
-    pub user: SwapUserContext<'info>,
-    /// The A token of the swap.
-    pub input_a: SwapToken<'info>,
-    /// The B token of the swap.
-    pub input_b: SwapToken<'info>,
-    /// The pool mint of the swap.
-    pub pool_mint: AccountInfo<'info>,
-    /// The output account for LP tokens.
-    pub output_lp: AccountInfo<'info>,
-}
 
-/// Accounts for a [crate::swap] instruction.
-#[derive(Accounts, Clone)]
-pub struct Swap<'info> {
-    /// The context of the user.
-    pub user: SwapUserContext<'info>,
-    /// Accounts for input tokens.
-    pub input: SwapToken<'info>,
-    /// Accounts for output tokens.
-    pub output: SwapOutput<'info>,
-}
 
-/// Accounts for a [crate::withdraw_one] instruction.
-#[derive(Accounts, Clone)]
-pub struct WithdrawOne<'info> {
-    /// The context of the user.
-    pub user: SwapUserContext<'info>,
-    /// The pool mint of the swap.
-    pub pool_mint: AccountInfo<'info>,
-    /// The input (user)'s LP token account
-    pub input_lp: AccountInfo<'info>,
-    /// The TokenAccount holding the swap's reserves of quote tokens; i.e., the token not being withdrawn.
-    ///
-    /// - If withdrawing token A, this is `swap_info.token_b.reserves`.
-    /// - If withdrawing token B, this is `swap_info.token_a.reserves`.
-    ///
-    /// These reserves are needed for the withdraw_one instruction since the
-    /// StableSwap `D` invariant requires both the base and quote reserves
-    /// to determine how many tokens are paid out to users withdrawing from
-    /// the swap.
-    ///
-    /// *For more info, see [stable_swap_client::state::SwapTokenInfo::reserves].*
-    pub quote_reserves: AccountInfo<'info>,
-    /// Accounts for output tokens.
-    pub output: SwapOutput<'info>,
-}
 
-/// Accounts for a [crate::withdraw] instruction.
-#[derive(Accounts, Clone)]
-pub struct Withdraw<'info> {
-    /// The context of the user.
-    pub user: SwapUserContext<'info>,
-    /// The input account for LP tokens.
-    pub input_lp: AccountInfo<'info>,
-    /// The pool mint of the swap.
-    pub pool_mint: AccountInfo<'info>,
-    /// The A token of the swap.
-    pub output_a: SwapOutput<'info>,
-    /// The B token of the swap.
-    pub output_b: SwapOutput<'info>,
-}
+
+
+
+
+
 
 /// Accounts for a [crate::set_fee_account] instruction.
 #[derive(Accounts, Clone)]
