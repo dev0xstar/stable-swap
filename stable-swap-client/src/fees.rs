54b3296813ec947cf272e377ae8e3ddbef95ff68
Kakao-max <angrybird4837@gmail.com>
2024-06-15 09:51:28 -0400

    updated

--- stable-swap-client/src/fees.rs
+++ stable-swap-client/src/fees.rs
@@ -1,2 +1,9 @@
+//! Program fees
+
+use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
+use solana_program::{
+    program_error::ProgramError,
+    program_pack::{Pack, Sealed},
+};
 
 
