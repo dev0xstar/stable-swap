255a87c9a3846490b61e4f280e22a13036f39f1c
Kakao-max <angrybird4837@gmail.com>
2024-06-15 09:52:04 -0400

    updated

--- stable-swap-client/src/fees.rs
+++ stable-swap-client/src/fees.rs
@@ -29,4 +29,54 @@ pub struct Fees {
     pub withdraw_fee_denominator: u64,
 }
 
+impl Sealed for Fees {}
+impl Pack for Fees {
+    const LEN: usize = 64;
+    fn unpack_from_slice(input: &[u8]) -> Result<Self, ProgramError> {
+        let input = array_ref![input, 0, 64];
+        #[allow(clippy::ptr_offset_with_cast)]
+        let (
+            admin_trade_fee_numerator,
+            admin_trade_fee_denominator,
+            admin_withdraw_fee_numerator,
+            admin_withdraw_fee_denominator,
+            trade_fee_numerator,
+            trade_fee_denominator,
+            withdraw_fee_numerator,
+            withdraw_fee_denominator,
+        ) = array_refs![input, 8, 8, 8, 8, 8, 8, 8, 8];
+        Ok(Self {
+            admin_trade_fee_numerator: u64::from_le_bytes(*admin_trade_fee_numerator),
+            admin_trade_fee_denominator: u64::from_le_bytes(*admin_trade_fee_denominator),
+            admin_withdraw_fee_numerator: u64::from_le_bytes(*admin_withdraw_fee_numerator),
+            admin_withdraw_fee_denominator: u64::from_le_bytes(*admin_withdraw_fee_denominator),
+            trade_fee_numerator: u64::from_le_bytes(*trade_fee_numerator),
+            trade_fee_denominator: u64::from_le_bytes(*trade_fee_denominator),
+            withdraw_fee_numerator: u64::from_le_bytes(*withdraw_fee_numerator),
+            withdraw_fee_denominator: u64::from_le_bytes(*withdraw_fee_denominator),
+        })
+    }
+
+    fn pack_into_slice(&self, output: &mut [u8]) {
+        let output = array_mut_ref![output, 0, 64];
+        let (
+            admin_trade_fee_numerator,
+            admin_trade_fee_denominator,
+            admin_withdraw_fee_numerator,
+            admin_withdraw_fee_denominator,
+            trade_fee_numerator,
+            trade_fee_denominator,
+            withdraw_fee_numerator,
+            withdraw_fee_denominator,
+        ) = mut_array_refs![output, 8, 8, 8, 8, 8, 8, 8, 8];
+        *admin_trade_fee_numerator = self.admin_trade_fee_numerator.to_le_bytes();
+        *admin_trade_fee_denominator = self.admin_trade_fee_denominator.to_le_bytes();
+        *admin_withdraw_fee_numerator = self.admin_withdraw_fee_numerator.to_le_bytes();
+        *admin_withdraw_fee_denominator = self.admin_withdraw_fee_denominator.to_le_bytes();
+        *trade_fee_numerator = self.trade_fee_numerator.to_le_bytes();
+        *trade_fee_denominator = self.trade_fee_denominator.to_le_bytes();
+        *withdraw_fee_numerator = self.withdraw_fee_numerator.to_le_bytes();
+        *withdraw_fee_denominator = self.withdraw_fee_denominator.to_le_bytes();
+    }
+}
 
