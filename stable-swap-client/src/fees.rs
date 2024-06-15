57f460d4102bbfae23653d7f73c34a86e54bc4d2
Kakao-max <angrybird4837@gmail.com>
2024-06-15 09:51:46 -0400

    updated

--- stable-swap-client/src/fees.rs
+++ stable-swap-client/src/fees.rs
@@ -6,4 +6,27 @@ use solana_program::{
     program_pack::{Pack, Sealed},
 };
 
+/// Fees struct
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+#[cfg_attr(feature = "fuzz", derive(arbitrary::Arbitrary))]
+pub struct Fees {
+    /// Admin trade fee numerator
+    pub admin_trade_fee_numerator: u64,
+    /// Admin trade fee denominator
+    pub admin_trade_fee_denominator: u64,
+    /// Admin withdraw fee numerator
+    pub admin_withdraw_fee_numerator: u64,
+    /// Admin withdraw fee denominator
+    pub admin_withdraw_fee_denominator: u64,
+    /// Trade fee numerator
+    pub trade_fee_numerator: u64,
+    /// Trade fee denominator
+    pub trade_fee_denominator: u64,
+    /// Withdraw fee numerator
+    pub withdraw_fee_numerator: u64,
+    /// Withdraw fee denominator
+    pub withdraw_fee_denominator: u64,
+}
+
 
